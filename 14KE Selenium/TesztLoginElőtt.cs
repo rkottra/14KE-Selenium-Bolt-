f0871c7cd86ff54f53a0044e60f5d270865efa78
Kottra Richárd <kottra.richard@jedlik.eu>
2023-03-23 17:31:15 +0100

    Add project files.

---
+++ 14KE Selenium/TesztLoginElőtt.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14KE_Selenium
+{
+    
+    internal class TesztLoginElőtt
+    {
+        FirefoxDriver driver;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            FirefoxOptions options = new FirefoxOptions();
+            options.AddArguments("--headless");
+            this.driver = new FirefoxDriver(options);
+            this.driver.Url = "http://localhost:4200/";
+        }
+
+        [Test]
+        public void FejlécTest()
+        {
+            IWebElement h1 = driver.FindElement(By.XPath("//header/h1"));
+            Assert.AreEqual(h1.Text, "Bolt");
+        }
+
+        [Test]
+        public void LoginAblakTest()
+        {
+            IWebElement legend = driver.FindElement(By.XPath("//fieldset/legend"));
+            Assert.AreEqual(legend.Text, "Login");
+        }
+
+        [Test]
+        public void EditButtonTest()
+        {
+             Assert.IsNull(FindMatIcon("//mat-icon"));
+        }
+
+        private IWebElement? FindMatIcon(string xpath)
+        {
+            try
+            {
+                return driver.FindElement(By.XPath(xpath));
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            this.driver.Quit();
+        }
+    }
+}
---
+++ 14KE Selenium/TesztLoginUtán.cs
@@ -0,0 +1,84 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14KE_Selenium
+{
+    
+    internal class TesztLoginUtán
+    {
+        FirefoxDriver driver;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            FirefoxOptions options = new FirefoxOptions();
+            options.AddArguments("--headless");
+            this.driver = new FirefoxDriver(options);
+            this.driver.Url = "http://localhost:4200/";
+
+            var wait = new WebDriverWait(this.driver, TimeSpan.FromSeconds(30));
+            var div = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("//fieldset/input[@type=\"button\"]")));
+
+            /*IJavaScriptExecutor js = (IJavaScriptExecutor)this.driver;
+            js.ExecuteScript("sessionStorage.removeItem('token');");
+            this.driver.Navigate().Refresh();*/
+
+            this.InteractLogin();
+            
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("//input[@value=\"Logout\"]")));
+        }
+
+        [Test]
+        public void FejlécTest()
+        {
+            IWebElement h1 = driver.FindElement(By.XPath("//header/h1"));
+            Assert.AreEqual(h1.Text, "Bolt");
+        }
+
+        [Test]
+        public void LoginAblakTest()
+        {
+            IWebElement logout = driver.FindElement(By.XPath("//fieldset/input[@type=\"button\"]"));
+            Assert.AreEqual(logout.GetAttribute("value"), "Logout");
+        }
+
+        [Test]
+        public void EditButtonTest()
+        {
+            Boolean matIconShow = driver.FindElement(By.XPath("//mat-icon")).Displayed;
+            Assert.IsTrue(matIconShow);
+            IWebElement matIcon = driver.FindElement(By.XPath("//mat-icon"));
+            Assert.AreEqual(matIcon.Text, "edit");
+        }
+
+        private void InteractLogin()
+        {
+            IWebElement inputEmail = driver.FindElement(By.Id("email"));
+            inputEmail.Clear();
+            inputEmail.SendKeys("kottra.richard@jedlik.eu");
+
+            IWebElement inputPassword = driver.FindElement(By.Id("password"));
+            inputPassword.Clear();
+            inputPassword.SendKeys("123");
+
+            IWebElement loginButton = driver.FindElement(By.XPath("//fieldset/input[@type=\"button\"]"));
+
+            loginButton.Click();
+
+            Thread.Sleep(1000);
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            this.driver.Quit();
+        }
+    }
+}
